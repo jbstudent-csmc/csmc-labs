e76ed00e90f2e9f2b31f2e62f001184888d46056
jbstudent-csmc <60447609+jbstudent-csmc@users.noreply.github.com>
2020-02-11 21:01:56 +0000

    Add files via upload

---
+++ Windchill/windchill.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+
+public class windchill
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	    double temp;               //Setting the temperature as a double.
+		double windSpeed;          //Setting the windSpeed as a double.
+		double windChill;          //Setting the windChill as a double.
+		
+		//Allowing input from the user via keyboard and setting input as keyboard.
+		Scanner keyboard = new Scanner(System.in);
+		
+		//Displays what type of program this is on the screen.
+		System.out.println("Wind Chill calculator.");
+		
+		//Prompts the user to enter the temperature in Fahrenheit.
+		System.out.println("Please enter the temperature in Fahrenheit."
+				+ "(Must be >=-45 and <=40):");
+		
+		//User inputs the temperature. The input is stored in temp.
+		temp = keyboard.nextDouble();
+		
+		//Prompts the user to enter the wind speed.
+		System.out.println("Please enter the wind speed.(Must be >=5 and <=60:)");
+		
+		//User inputs the wind speed. The input is stored in windSpeed
+		windSpeed = keyboard.nextDouble();
+		
+		//Formula to find out the wind chill.
+		windChill = 35.74 + (0.6215 * temp) - (35.75 * (Math.pow(windSpeed,0.16))) + (0.4275 * ( temp *(Math.pow(windSpeed,0.16))));
+		
+		//Displays the wind chill.
+		System.out.println("Wind chill temperature is " + windChill + " degrees Fahrenheit\n");
+		
+		//Displays the Programmer.
+		System.out.println("Programmer: Jon Howard");
+		
+		keyboard.close();
+		
+		
+	}
+}
