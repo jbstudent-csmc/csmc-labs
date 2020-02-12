8b4db1b0f94b9a8344b8552f1bf810237700e3f9
jbstudent-csmc <60447609+jbstudent-csmc@users.noreply.github.com>
2020-02-12 03:50:43 +0000

    Add files via upload
    Updated windchill program with corrections.

--- Windchill/windchill.java
+++ Windchill/windchill.java
@@ -1,7 +1,10 @@
-
+package windchill;
 import java.util.Scanner;
 
 public class windchill
+/**
+ * Programmer: Jon Howard
+ */
 {
 
 	public static void main(String[] args) 
@@ -24,12 +27,26 @@ public class windchill
 		
 		//User inputs the temperature. The input is stored in temp.
 		temp = keyboard.nextDouble();
+		if(!(temp >= -45) || !(temp <= 40) )
+		{
+			System.out.println("Incorrect values. Please enter a number between >=-45 and <=40");
+			temp =keyboard.nextDouble();
+		}
+			
+		
 		
 		//Prompts the user to enter the wind speed.
 		System.out.println("Please enter the wind speed.(Must be >=5 and <=60:)");
 		
 		//User inputs the wind speed. The input is stored in windSpeed
 		windSpeed = keyboard.nextDouble();
+		
+		if(!(windSpeed >= 5) || !(windSpeed <= 60) )
+		{
+			System.out.println("Incorrect values. Please enter a number between >=-5 and <=60");
+			windSpeed =keyboard.nextDouble();
+		}
+			
 		
 		//Formula to find out the wind chill.
 		windChill = 35.74 + (0.6215 * temp) - (35.75 * (Math.pow(windSpeed,0.16))) + (0.4275 * ( temp *(Math.pow(windSpeed,0.16))));
